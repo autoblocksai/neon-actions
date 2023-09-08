e90fa2e529eec024f0cf63ee632c68f812f51494
Nicole White <nicole@autoblocks.ai>
2023-09-08 00:45:21 -0400

    Add neon branching actions

---
+++ neon/src/delete-branches.ts
@@ -0,0 +1,25 @@
+import { NeonSDK, makeNeonBranchNamePrefix, sleep } from './util';
+
+const main = async () => {
+  const neon = new NeonSDK();
+  const neonBranchNamePrefix = makeNeonBranchNamePrefix();
+  const branches = await neon.getBranches();
+
+  console.log(`Found ${branches.length} branches:`);
+  branches.forEach((branch) => {
+    console.log(`  ${branch.name} (${branch.id})`);
+  });
+
+  const branchesToDelete = branches.filter((branch) =>
+    branch.name.startsWith(neonBranchNamePrefix),
+  );
+
+  console.log(`Deleting branches with prefix '${neonBranchNamePrefix}':`);
+  for (const branch of branchesToDelete) {
+    console.log(`  deleting ${branch.name} (${branch.id})`);
+    await neon.deleteBranch(branch.id);
+    await sleep(1000); // Sleep between deletes to be polite
+  }
+};
+
+main();
